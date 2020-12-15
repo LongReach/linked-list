eba070025b4682a4c7b9af62569230c55d757dac
Ryan McMahon <rmcmahon75@gmail.com>
2020-12-15 00:37:00 -0500

    More test improvements Added find function Way of moving to an arbitrary node is more efficient

--- LinkedListTest.py
+++ LinkedListTest.py
@@ -1,112 +1,215 @@
 import random
+import time
+import argparse
 from linked_list import LinkedList
 
-validity_failure = False
+parser = argparse.ArgumentParser(description='Tester program for LinkedListClass.')
+parser.add_argument("-v", "--verbose", help="If set, print extra info", action="store_true")
+parser.add_argument("--seed", help="A seed for random number generation (to reproduce same tests)", type=int, default=-1)
+args = parser.parse_args()
 
-def print_list_details(the_list, operation=None):
+validity_failure = False
+failed_tests = []
+verbose_test = args.verbose
+random_seed = args.seed
+
+if random_seed == -1:
+    # choose a seed at random (sort of)
+    random_seed = int(time.time()) % 100000
+random.seed(random_seed)
+
+
+# Given a linked list, print information about it, validate the list, and compare to contents of a Python list
+# last_operation_str: string containing name of last last_operation_str
+# expected_list: if set, a Python list containing expected results of last last_operation_str
+def examine_list_details(the_list, last_operation_str=None, expected_list=None):
     global validity_failure
     valid, error_str = the_list._validate()
-    if operation is not None:
-        print("operation:", operation)
-    print("list is:", the_list.get_items())
-    print("    length is:", the_list.size())
-    print("    cached index:", the_list.cached_index)
-    print("    cached item:", "NONE" if the_list.cached_node is None else the_list.cached_node.item)
+    if last_operation_str is not None:
+        print("last_operation_str:", last_operation_str)
+    if verbose_test:
+        print("list is:", the_list.get_items())
+        print("    length is:", the_list.size())
+        print("    cached index:", the_list.cached_index)
+        print("    cached item:", "NONE" if the_list.cached_node is None else the_list.cached_node.item)
     if not valid:
         validity_failure = True
         print("    *** validity failed ***, error code:", error_str)
-    print("--------------------")
+    if expected_list is not None:
+        match = True
+        the_items = the_list.get_items()
+        if len(expected_list) != len(the_items):
+            match = False
+        else:
+            for i, item in enumerate(expected_list):
+                if expected_list[i] != the_items[i]:
+                    match = False
+        if not match:
+            validity_failure = True
+            print("    *** expected result failure ***:", expected_list)
+    if verbose_test:
+        print("--------------------")
 
 print("")
-print("LIST ONE")
+print("TEST ONE")
 words = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot"]
-ll = LinkedList(words)
-ll._new_cache_item(3)
-print_list_details(ll)
-ll.pop_head()
-print_list_details(ll, "popped head")
-ll.pop_tail()
-print_list_details(ll, "popped tail")
-ll.add_head("A")
-print_list_details(ll, "added head")
-ll.add_tail("F")
-print_list_details(ll, "added tail")
+callsign_ll = LinkedList(words)
+callsign_ll._new_cache_item(3)
+examine_list_details(callsign_ll, "new list, cache item 3")
+callsign_ll.reverse_list()
+examine_list_details(callsign_ll, "reversed")
+callsign_ll.reverse_list()
+callsign_ll.pop_head()
+examine_list_details(callsign_ll, "popped head")
+callsign_ll.pop_tail()
+examine_list_details(callsign_ll, "popped tail", ['bravo', 'charlie', 'delta', 'echo'])
+callsign_ll.add_head("A")
+examine_list_details(callsign_ll, "added head")
+callsign_ll.add_tail("F")
+examine_list_details(callsign_ll, "added tail")
+if validity_failure:
+    failed_tests.append("TEST ONE")
+    validity_failure = False
 
 print("")
-print("LIST TWO")
-ll2 = LinkedList()
-ll2.add_head("two")
-ll2.add_head("one")
-ll2._new_cache_item(0)
-print_list_details(ll2)
-ll2.pop_head()
-print_list_details(ll2, "pop head")
-ll2.pop_head()
-print_list_details(ll2, "pop head")
-ll2.add_head("zzz")
-print_list_details(ll2, "add head")
+print("TEST TWO")
+number_ll = LinkedList()
+number_ll.add_head("two")
+number_ll.add_head("one")
+number_ll._new_cache_item(0)
+examine_list_details(number_ll)
+number_ll.pop_head()
+examine_list_details(number_ll, "pop head")
+number_ll.pop_head()
+examine_list_details(number_ll, "pop head", expected_list=[])
+number_ll.add_head("zzz")
+examine_list_details(number_ll, "add head")
+if validity_failure:
+    failed_tests.append("TEST TWO")
+    validity_failure = False
 
 print("")
-print("LIST THREE")
-ll3 = LinkedList(["apple", "orange", "pear", "banana", "grape", "lemon", "lime", "grapefruit"])
-ll3.get_item(2)
-print_list_details(ll3, "get item 2")
-ll3.get_item(5)
-print_list_details(ll3, "get item 5")
-ll3.get_item(3)
-print_list_details(ll3, "get item 3")
-ll3.insert("coconut", 3)
-print_list_details(ll3, "insert")
-ll3.insert("tomato", 0)
-print_list_details(ll3, "insert")
-try:
-    ll3.get_item(17)
-except IndexError:
-    print("index out of range, as expected")
+print("TEST THREE")
+fruit_ll = LinkedList(["apple", "orange", "pear", "banana", "grape", "lemon", "lime", "grapefruit"])
+fruit_ll.get_item(2)
+examine_list_details(fruit_ll, "get item 2")
+fruit_ll.get_item(5)
+examine_list_details(fruit_ll, "get item 5")
+fruit_ll.get_item(3)
+examine_list_details(fruit_ll, "get item 3")
+fruit_ll.insert("coconut", 3)
+examine_list_details(fruit_ll, "insert", ['apple', 'orange', 'pear', 'coconut', 'banana', 'grape', 'lemon', 'lime', 'grapefruit'])
+fruit_ll.insert("tomato", 0)
+examine_list_details(fruit_ll, "insert", ['tomato', 'apple', 'orange', 'pear', 'coconut', 'banana', 'grape', 'lemon', 'lime', 'grapefruit'])
+bad_indices = [17, -1, 500, -3]
+for bi in bad_indices:
+    try:
+        fruit_ll.get_item(bi)
+    except IndexError:
+        print("index out of range, as expected")
+    else:
+        print("did not fail index-out-of-range test, as expected")
+        validity_failure = True
+if validity_failure:
+    failed_tests.append("TEST THREE")
+    validity_failure = False
 
 print("")
-print("LIST RANDOM TEST")
-rand_list = LinkedList()
+print("FIND TEST")
+# We should have: ['tomato', 'apple', 'orange', 'pear', 'coconut', 'banana', 'grape', 'lemon', 'lime', 'grapefruit']
+# Each tuple: item, start index, reverse
+valid_finds = [("coconut", 0, False), ("apple", 4, True), ("lime", 3, False), ("pear", -1, True)]
+fail_finds = [("apple", 4, False), ("sandwich", -1, False), ("kiwi", -1, True), ("lemon", 4, True), ("banana", 77, False)]
+finds = [valid_finds, fail_finds]
+for find_list in finds:
+    for f in find_list:
+        try:
+            find_str = "good find:" if find_list is valid_finds else "bad find:"
+            print("{} {} starting from {}{}".format(find_str, f[0], f[1], ", reverse=True" if f[2] else ""))
+            fruit_ll.find_item(f[0], f[1], f[2])
+        except (ValueError, IndexError):
+            if find_list is valid_finds:
+                print("failed find for {} where success was expected".format(f[0]))
+        else:
+            if find_list is fail_finds:
+                print("successful find for {} where failure was expected".format(f[0]))
+if validity_failure:
+    failed_tests.append("FIND TEST")
+    validity_failure = False
+
+# This test creates an empty list, then performs a series of random operations on it, putting in random numbers
+# At the same time, we perform the same operations on a regular Python list, which we compare
+print("")
+print("RANDOM TEST: seed={}".format(random_seed))
+rand_ll = LinkedList()
+python_list = []
 for i in range(20):
     random_num = random.randrange(10000)
     operations = ["add_head", "add_tail", "pop_head", "pop_tail", "insert"]
     rand_op = random.randrange(5)
     rand_index = 0
     if rand_op == 0:
-        rand_list.add_head(random_num)
+        rand_ll.add_head(random_num)
+        python_list.insert(0, random_num)
     elif rand_op == 1:
-        rand_list.add_tail(random_num)
+        rand_ll.add_tail(random_num)
+        python_list.append(random_num)
     elif rand_op == 2:
-        rand_list.pop_head()
+        rand_ll.pop_head()
+        if len(python_list) > 0:
+            python_list.pop(0)
     elif rand_op == 3:
-        rand_list.pop_tail()
+        rand_ll.pop_tail()
+        if len(python_list) > 0:
+            python_list.pop()
     elif rand_op == 4:
-        rand_index = 0 if rand_list.size() == 0 else random.randrange(rand_list.size())
-        rand_list.insert(random_num, rand_index)
+        rand_index = 0 if rand_ll.size() == 0 else random.randrange(rand_ll.size())
+        rand_ll.insert(random_num, rand_index)
+        python_list.insert(rand_index, random_num)
     op_str = operations[rand_op]
     if rand_op == 0 or rand_op == 1 or rand_op == 4:
         op_str = op_str + " " + str(random_num)
         if rand_op == 4:
             op_str = op_str + " at index " + str(rand_index)
-    print_list_details(rand_list, op_str)
-
+    examine_list_details(rand_ll, op_str, python_list)
 if validity_failure:
-    print("")
-    print("VALIDITY FAILURE")
+    failed_tests.append("RANDOM TEST")
+    validity_failure = False
 
 print("")
-print("LIST RANDOM SEARCHES")
-
+print("RANDOM SEARCHES, seed={}".format(random_seed))
 # Now add some more random numbers
 for i in range(10):
     random_num = random.randrange(10000)
-    rand_list.add_tail(random_num)
-print_list_details(rand_list)
+    rand_ll.add_tail(random_num)
+    python_list.append(random_num)
+examine_list_details(rand_ll, expected_list=python_list)
+print("linked list", rand_ll.get_items())
+print("python list", python_list)
 
 for i in range(10):
-    rand_index = random.randrange(rand_list.size())
-    item = rand_list.get_item(rand_index)
-    print("Item at {} is:".format(rand_index), item)
-    valid, error_str = rand_list._validate()
+    rand_index = random.randrange(rand_ll.size())
+    old_cached_index = rand_ll.cached_index
+    item = rand_ll.get_item(rand_index)
+    if verbose_test:
+        print("Item at {} is:".format(rand_index), item, "cached index moves from {} to {}".format(old_cached_index, rand_ll.cached_index))
+    else:
+        print("Item at {} is:".format(rand_index), item)
+    valid, error_str = rand_ll._validate()
     if not valid:
         print("Validity error:", error_str)
+        validity_failure = True
+    # make sure that retrieved item matches item in python list
+    if python_list[rand_ll.cached_index] != item:
+        print("Cache index is wrong")
+        validity_failure = True
+if validity_failure:
+    failed_tests.append("RANDOM SEARCHES")
+    validity_failure = False
+
+if len(failed_tests) > 0:
+    print("")
+    print("FAILED TESTS")
+    for t in failed_tests:
+        print(t)
+
--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,7 @@
+# Implementation of a linked list. It is doubly-linked. There is a caching feature, for keeping track of last
+# node accessed. That way, if we try to get an item at a particular index, we might be able to get to it
+# more quickly.
+
 class ListNode(object):
 
     def __init__(self, item):
@@ -6,6 +10,16 @@ class ListNode(object):
         self.prev = None
 
 class LinkedList(object):
+
+    class NodeRef:
+        def __init__(self, node=None, index=-1):
+            self.node = node
+            self.index = index
+
+        def clear(self):
+            self.node = None
+            self.index = -1
+
 
     def __init__(self, iterable=None):
         self.head = None
@@ -19,6 +33,29 @@ class LinkedList(object):
             for i in iterable:
                 self.add_tail(i)
 
+    # Empties the list
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        self.cached_node = None
+        self.cached_index = -1
+
+    # Reverses the list in place
+    def reverse_list(self):
+        node = self.head
+        while node:
+            orig_next = node.next
+            node.next = node.prev
+            node.prev = orig_next
+            node = orig_next
+        orig_tail = self.tail
+        self.tail = self.head
+        self.head = orig_tail
+        if self.cached_index != -1:
+            self.cached_index = self.length - 1 - self.cached_index
+
+    # Adds item to head of linked list
     def add_head(self, item):
         node = ListNode(item)
         if self.head is None:
@@ -31,6 +68,7 @@ class LinkedList(object):
         self.length = self.length + 1
         self._adjust_cache(True, 0)
 
+    # Adds item to tail of linked list
     def add_tail(self, item):
         node = ListNode(item)
         if self.tail is None:
@@ -43,6 +81,7 @@ class LinkedList(object):
         self.length = self.length + 1
         self._adjust_cache(True, self.length-1)
 
+    # Inserts item into list, before item at specified index. If index == length of list, place after last item.
     def insert(self, item, index=0):
         if index < 0 or index > self.length:
             raise IndexError("linked list index out of range")
@@ -62,6 +101,7 @@ class LinkedList(object):
             self.cached_node = new_node
             self.cached_index = index
 
+    # Pops item from head of list, returns item
     def pop_head(self):
         if self.head is None: return None
         ret_node = self.head
@@ -74,6 +114,7 @@ class LinkedList(object):
         self._adjust_cache(False, 0)
         return ret_node.item
 
+    # Pops item from tail of list, returns item
     def pop_tail(self):
         if self.tail is None: return None
         ret_node = self.tail
@@ -86,9 +127,11 @@ class LinkedList(object):
         self._adjust_cache(False, self.length)
         return ret_node.item
 
+    # Returns current size of list
     def size(self):
         return self.length
 
+    # Gets item at index. Tries to use caching for extra speed.
     def get_item(self, index):
         if index < 0 or index >= self.length:
             raise IndexError("linked list index out of range")
@@ -97,6 +140,29 @@ class LinkedList(object):
         self.cached_index = index
         return node.item
 
+    # Locates item in list, starting from start_index.
+    # Params
+    #   item: item to find
+    #   start_index: if given, index to start searching from. -1 is the same as not giving an index.
+    #   backwards: if true, we search backwards
+    # Returns index of item
+    def find_item(self, item, start_index=None, backwards=False):
+        if start_index is None or start_index == -1:
+            start_index = self.length-1 if backwards else 0
+        if start_index < 0 or start_index >= self.length:
+            raise IndexError("linked list index out of range")
+        node = self._get_to_index(start_index)
+        idx = start_index
+        while node is not None:
+            if node.item == item:
+                self.cached_node = node
+                self.cached_index = idx
+                return idx
+            idx = idx + (-1 if backwards else 1)
+            node = node.prev if backwards else node.next
+        raise ValueError("Item not found in linked list")
+
+    # Returns a Python list of items in list
     def get_items(self):
         ret_list = []
         node = self.head
@@ -105,6 +171,7 @@ class LinkedList(object):
             node = node.next
         return ret_list
 
+    # For debugging purposes, changes the cached node
     def _new_cache_item(self, idx):
         if self.length == 0: return
         node = self.head
@@ -113,39 +180,56 @@ class LinkedList(object):
         self.cached_node = node
         self.cached_index = idx
 
+    # Returns node at target_idx, takes advantage of caching
     def _get_to_index(self, target_idx):
-        start_idx = 0
-        step = 1
-        node = self.head
+        # The idea is to pick the closest starting point: head of list, tail, or cached node
+        # Each tuple: a delta value, a starting index, node
+        options = [(target_idx, 0, self.head), # representing start of linked list
+                         (target_idx - (self.length-1), self.length-1, self.tail)] # end of l. list
+
         if self.cached_index != -1:
-            delta = target_idx - self.cached_index
-            if abs(delta) < target_idx:
-                # it is faster to start from the cached index
-                start_idx = self.cached_index
-                step = 1 if delta >= 0 else -1
-                node = self.cached_node
-        idx_steps = str(node.item)
-        for i in range(start_idx, target_idx, step):
+            # There is a cached node
+            options.append((target_idx - self.cached_index, self.cached_index, self.cached_node))
+
+        # Find best option
+        best_delta = self.length
+        best_op = options[0]
+        for tup in options:
+            if abs(tup[0]) < best_delta:
+                best_delta = abs(tup[0])
+                best_op = tup
+
+        node = best_op[2]
+        step = -1 if best_op[0] < 0 else 1 # delta to target
+        for i in range(best_op[1], target_idx, step):
             node = node.next if (step == 1) else node.prev
-            idx_steps = idx_steps + " " + str(node.item)
-        #print("get to index steps:", idx_steps)
+
         return node
 
-    # item_added: True if item was just added, False if remove
+    # If an item is added to or removed from linked list, we need to change index of cached node
+    # item_added: True if item was just added, False if removed
     # item_index: which item was just added or subtracted
     def _adjust_cache(self, item_added, item_index):
-        if self.cached_index == -1: return
+        if self.cached_index == -1:
+            # We don't have a cached node, so pick one and exit
+            if self.length > 0:
+                target_idx = int(self.length / 2) # go to middle of list
+                self.cached_node = self._get_to_index(target_idx)
+                self.cached_index = target_idx
+            return
+
         if item_added:
             if item_index <= self.cached_index:
-                self.cached_index = self.cached_index + 1
+                self.cached_index = self.cached_index + 1 # cached node index pushed to right
         else:
             # item removed
             if self.length == 0:
+                # The list is now empty
                 self.cached_index = -1
                 self.cached_node = None
             else:
                 if item_index == self.cached_index:
-                    # We deleted the cached node
+                    # Cached node pointer was pointing to node that got deleted
                     self.cached_node = None
                     self.cached_index = -1
                 elif item_index < self.cached_index:
@@ -153,6 +237,7 @@ class LinkedList(object):
         if self.cached_index >= self.length:
             self.cached_index = self.length - 1
 
+    # Debugging feature; tests the linked list for validity. Returns False if list invalid, error code string
     def _validate(self):
         error_str = ""
         count, node = 0, self.head
